81f998a55c68101961a14bd72f94bfe90bad2c7e
Petikov Vladimir <developer-krox@yandex.ru>
2023-02-28 18:28:56 +0300

    280223-1830 Project Movies-2 c Webpack - 10

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,38 +4,41 @@ import {Movies} from "../Movies/Movies";
 import styles from "./Main.module.scss"
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
-import {PaginationItem} from "@mui/material";
+import {query_kino_non_official} from './Query'
 
 
-const API_KEY = "cd73c1bd-276a-4e67-9aab-fe1f28627864";
+// const API_KEY = "cd73c1bd-276a-4e67-9aab-fe1f28627864";
+// const API_KEY2 ="TZECVJG-4A248BP-NV72AKJ-W2SGX7S";
 // const API_URL_POPULAR = "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1";
-const API_URL = "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?&page=";
+// const API_URL = "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?&page=";
+// const API_URL2 = "https://api.kinopoisk.dev/v1/movie?page=";
 class Main extends React.Component {
 
     state = {
         movies: [],
-        pagesCount: 0,
+        pagesCount: 20,
         currentPage: 1
     }
 
     componentDidMount() {
-        fetch(API_URL + this.state.currentPage, {
-            headers: {
-                "Content-Type": "application/json",
-                "X-API-KEY": API_KEY,
-            },
-        }).then((responce) => responce.json())
-          .then((data) => this.setState({ movies: data.films, pagesCount: data.pagesCount}))
-    }
-    // componentDidUpdate(prevProps, prevState, snapshot) {
+    //   console.log(API_URL + this.state.currentPage)
     //     fetch(API_URL + this.state.currentPage, {
     //         headers: {
     //             "Content-Type": "application/json",
     //             "X-API-KEY": API_KEY,
     //         },
     //     }).then((responce) => responce.json())
-    //         .then((data) => this.setState({ movies: data.films, pagesCount: data.pagesCount}))
-    // }
+    //       .then((data) => this.setState({ movies: data.films, pagesCount: data.pagesCount}))
+    //     query_kino_non_official();
+    }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevState.currentPage !== this.state.currentPage) {
+            alert("Bingo!!!")
+            //query_kino_non_official();
+        }
+
+
+    }
 
 
 
@@ -43,16 +46,16 @@ class Main extends React.Component {
        const styleMain =[styles.content, styles.container].join(' ');
        const stylePagination =[styles.pagination, styles.container].join(' ');
        const { movies, pagesCount, currentPage } = this.state;
-       // const West = () => {
-       //    // const [Page, setPage] = useState(1);
-       //   //  console.log(PaginationItem(pagesCount))
-       // }
+       const West = () => {
+          // const [Page, setPage] = useState(1);
+         //  console.log(PaginationItem(pagesCount))
+       }
 
         //console.log(pagesCount);
-       // const handleChange = (event, p) => {
-       //      console.log(event, p)
-       //      this.setState({currentPage: p})
-       // }
+       const handleChange = (event, p) => {
+            console.log(event, p)
+            this.setState({currentPage: p})
+       }
 
        return (
 
@@ -63,17 +66,16 @@ class Main extends React.Component {
                                    color="primary"
                                    className={stylePagination}
                                    page={currentPage}
-                                   // onClick={West}
-                                   // onChange={handleChange}
+                                   onChange={handleChange}
                        />
                    </Stack>
-                   <div className={styleMain}>
-                       {movies.length ? (
-                           <Movies movies={movies} />
-                       ) : (
-                           <h3>Загрузка...</h3>
-                       )}
-                   </div>
+                   {/*<div className={styleMain}>*/}
+                   {/*    {movies.length ? (*/}
+                   {/*        <Movies movies={movies} />*/}
+                   {/*    ) : (*/}
+                   {/*        <h3>Загрузка...</h3>*/}
+                   {/*    )}*/}
+                   {/*</div>*/}
 
                </main>
            )
